768e9c0719ffdd9d873a0b63a25bfbcac6bd8b0d
shaun <shaun@cmune.com>
2017-09-09 14:40:49 -0700

    Added server DateTime as UTC to AppConfig model, so clients can determine start and finish time of promotions.

--- Assets/Plugins/HappyTokenApi.Models/AppConfig.cs
+++ Assets/Plugins/HappyTokenApi.Models/AppConfig.cs
@@ -5,6 +5,8 @@ namespace HappyTokenApi.Models
 {
     public class AppConfig
     {
+        public DateTime ServerDateTime { get; set; }
+
         public AppDefaults AppDefaults { get; set; }
 
         public List<Avatar> Avatars { get; set; }
